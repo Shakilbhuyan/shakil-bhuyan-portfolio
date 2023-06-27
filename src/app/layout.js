90f7257e76217b344d051ba3b22fe3e91d783cd6
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-06-27 19:01:35 +0600

    naver complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Navber from '@/Components/NavBer/Navber'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,7 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+       <div className='max-w-screen-xl mx-auto'>
+       <div>
+          <Navber></Navber>
+        </div>
         {children}
+       </div>
         </body>
     </html>
   )
