0570d006fa6b460d9b296f84e8a1423d19cc9080
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-09-18 21:45:22 +0600

    added animantion

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,31 @@ import Skill from '@/Components/SkillsAndProjects/Skill'
 import About from '@/Components/about/About'
 import Banner from '@/Components/banner/Banner'
 import Contact from '@/Components/contact/Contact'
+import { Fade, Slide } from 'react-reveal';
 
 
 
 
 
 export default function Home() {
-  
+
   return (
     <div>
-      <Banner></Banner>
+      <Fade bottom cascade>
+        <Banner></Banner>
+        <Slide left>
+          <About></About>
+        </Slide>
+        <Slide right>
+          <Skill></Skill>
+        </Slide>
+        <Contact></Contact>
+      </Fade>
+
+      {/* <Banner></Banner>
       <About></About>
       <Skill></Skill>
-     <Contact></Contact>
+     <Contact></Contact> */}
     </div>
   )
 }
